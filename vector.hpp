d9192a38d49cf291c30205ddf97cfa65fb4ef100
wkddlstn1321 <wkddlstn132@gmail.com>
2022-12-09 20:13:16 +0900

    vector iterator clear

--- vector.hpp
+++ vector.hpp
@@ -7,6 +7,7 @@
 #include "type_traits.hpp"
 #include "random_access_iterator.hpp"
 #include "algorithm.hpp"
+#include "reverse_iterator.hpp"
 
 namespace ft
 {
@@ -14,18 +15,18 @@ namespace ft
 	class vector
 	{
 	public:
-		typedef T												value_type;
-		typedef Alloc											allocator_type;
-		typedef typename allocator_type::reference				reference;
-		typedef typename allocator_type::const_reference		const_reference;
-		typedef typename allocator_type::pointer				pointer;
-		typedef typename allocator_type::const_pointer			const_pointer;
-		typedef ft::random_access_iterator<value_type>			iterator;
-		// typedef ft::random_access_iterator<const_value_type>	const_iterator;
-		// typedef ft::reverse_iterator<iterator>					reverse_iterator;
-		// typedef ft::reverse_iterator<const_iterator>			const_reverse_iterator;
+		typedef T														value_type;
+		typedef Alloc													allocator_type;
+		typedef typename allocator_type::reference						reference;
+		typedef typename allocator_type::const_reference				const_reference;
+		typedef typename allocator_type::pointer						pointer;
+		typedef typename allocator_type::const_pointer					const_pointer;
+		typedef ft::random_access_iterator<value_type>					iterator;
+		typedef ft::random_access_iterator<const value_type>			const_iterator;
+		typedef ft::reverse_iterator<iterator>							reverse_iterator;
+		typedef ft::reverse_iterator<const_iterator>					const_reverse_iterator;
 		typedef typename ft::iterator_traits<iterator>::difference_type	difference_type;
-		typedef typename allocator_type::size_type				size_type;
+		typedef typename allocator_type::size_type						size_type;
 
 	//friend keyword need protected not private
 	protected:
@@ -84,6 +85,40 @@ namespace ft
 			while (size_type i = 0 ; i < n ; i++)
 				this->_alloc.destory(this->_data[i]);
 			this->_alloc.deallocate(this->_data, this->capacity);
+		}
+
+		//iterator
+		iterator begin()
+		{
+			return (this->_data);
+		}
+		const_iterator begin() const
+		{
+			return (this->_data);
+		}
+		iterator end()
+		{
+			return (this->_data + this->_size);
+		}
+		const_iterator end() const
+		{
+			return (this->_data + this->_size);
+		}
+		reverse_iterator rbegin()
+		{
+			return (end());
+		}
+		const_reverse_iterator rbegin() const
+		{
+			return(end());
+		}
+		reverse_iterator rend()
+		{
+			return(begin());
+		}
+		const_reverse_iterator rend() const
+		{
+			return(begin());
 		}
 
 		//Capacity
@@ -235,7 +270,7 @@ namespace ft
 		void pop_back()
 		{
 			this->_size--;
-			this->_alloc.destory(this->_data[this->_size - 1]);
+			this->_alloc.destory(this->_data[this->_size]);
 		}
 		iterator insert(iterator position, const value_type &val)
 		{
@@ -248,22 +283,26 @@ namespace ft
 			// this->_size++;
 
 		}
-		void insert(iterator position, size_type n, const value_type &val)
-		{
+		// void insert(iterator position, size_type n, const value_type &val)
+		// {
 
-		}
-		template <class InputIterator>
-		void insert(iterator position, InputIterator first, InputIterator last)
-		{
+		// }
+		// template <class InputIterator>
+		// void insert(iterator position, InputIterator first, InputIterator last)
+		// {
 
-		}
+		// }
 		iterator erase(iterator position)
 		{
-			size_type n = position - this->_data;
+			if (position + 1 != end())
+				std::copy(position + 1, end(), position);
 			this->_size--;
+			this->_alloc.destory(this->_data[this->_size]);
+			return (position);
 		}
 		iterator erase(iterator first, iterator last)
 		{
+
 		}
 		void swap(vector &x)
 		{
