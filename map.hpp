4b4b5979f6343036692414d1d98e17a311427d17
wkddlstn1321 <wkddlstn132@gmail.com>
2022-12-20 22:56:06 +0900

    map very hard

--- map.hpp
+++ map.hpp
@@ -11,35 +11,56 @@ namespace ft
 	template <class Key,											// map::key_type
 			  class T,												// map::mapped_type
 			  class Compare = std::less<Key>,						// map::key_compare
-			  class Alloc = std::allocator<ft::pair<const Key, T>>> // map::allocator_type
+			  class Alloc = std::allocator<ft::pair<const Key, T>> >// map::allocator_type
 	class map
 	{
 	public:
-		typedef Key														key_type;
-		typedef T														mapped_type;
-		typedef ft::pair<const Key, T>									value_type;
-		typedef Compare													key_compare;
-		typedef Alloc													allocator_type;
-		typedef typename allocator_type::reference						reference;
-		typedef typename allocator_type::const_reference				const_reference;
-		typedef typename allocator_type::pointer						pointer;
-		typedef typename allocator_type::const_pointer					const_pointer;
-		typedef typename ft::bidirectional_iterator<value_type>			iterator;
-		typedef typename ft::bidirectional_iterator<const value_type>	const_iterator;
-		typedef typename ft::reverse_iterator<iterator>					reverse_iterator;
-		typedef typename ft::reverse_iterator<const_iterator>			const_reverse_iterator;
-		typedef std::ptrdiff_t											difference_type;
-		typedef std::size_t												size_type;
+		typedef Key															key_type;
+		typedef T															mapped_type;
+		typedef ft::pair<const Key, T>										value_type;
+		typedef Compare														key_compare;
+		typedef Alloc														allocator_type;
+		typedef typename allocator_type::reference							reference;
+		typedef typename allocator_type::const_reference					const_reference;
+		typedef typename allocator_type::pointer							pointer;
+		typedef typename allocator_type::const_pointer						const_pointer;
+		typedef typename ft::bidirectional_iterator<value_type>				iterator;
+		typedef typename ft::bidirectional_iterator<const value_type>		const_iterator;
+		typedef typename ft::reverse_iterator<iterator>						reverse_iterator;
+		typedef typename ft::reverse_iterator<const_iterator>				const_reverse_iterator;
+		typedef std::ptrdiff_t												difference_type;
+		typedef std::size_t													size_type;
 
-		typedef typename allocator_type::template rebind<value_type>::other type_allocator;
-		// typedef std::allocator_traits<type_allocator> type_traits;
-		
+		typedef typename allocator_type::template rebind<value_type>::other	type_allocator;
+		typedef std::allocator_traits<type_allocator>						type_traits;
 
-		//Member func
-		//construct
+	private:
+		key_compare		_comp;
+		allocator_type	_alloc;
+
+	public:
+		//value_compare class~
+		template <class Key, class T, class Compare, class Alloc>
+		class map<Key, T, Compare, Alloc>::value_compare
+		{ // in C++98, it is required to inherit binary_function<value_type,value_type,bool>
+			friend class map;
+		protected:
+			Compare comp;
+			value_compare(Compare c) : comp(c) {} // constructed with map's comparison object
+		public:
+			typedef bool		result_type;
+			typedef value_type	first_argument_type;
+			typedef value_type	second_argument_type;
+			bool operator()(const value_type &x, const value_type &y) const
+			{
+				return comp(x.first, y.first);
+			}
+		};
+		// Member func
+		// construct
 		explicit map(const key_compare &comp = key_compare(), const allocator_type &alloc = allocator_type())
+		: _comp(comp), _alloc(alloc)
 		{
-
 		}
 		template <class InputIterator>
 		map(InputIterator first, InputIterator last, const key_compare &comp = key_compare(), const allocator_type &alloc = allocator_type())
